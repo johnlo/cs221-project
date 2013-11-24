77f9ba5bdb75ca67c859ff60f692919a26e27c8d
John Lo <johnlo@stanford.edu>
2013-11-24 00:23:41 -0800

    Implement line length constraint.

--- poem.py
+++ poem.py
@@ -1,76 +1,89 @@
 import os
-from solver import *
+import solver
 from collections import Counter
 
-class PoemCSP():
-	def __init__(self, path, n, mood):
-		self.poems = {}
+class PoemCSP(solver.CSP):
+    def __init__(self, path, ngramSize, mood):
+        solver.CSP.__init__(self)
+        self.poems = {}
+        poem_lengths = Counter()
+        line_lengths = Counter()
+        for poemdir in os.listdir(path):
+            self.poems[poemdir] = \
+                open(os.path.join(path, poemdir, 'text')).read().replace(
+                '?', ' ?').replace('!', ' !').replace('.', ' .')
+            moods = open(os.path.join(path, poemdir, 'mood')).read().split()
+            if mood in moods:
+                poem_lengths[len(self.poems[poemdir].split())] += 1
+                for line in self.poems[poemdir].split('\n'):
+                    length = len(line.split())
+                    if length > 0:
+                        line_lengths[length] += 1
+        self.poemLength = poem_lengths.most_common(1)[0][0]
+        self.lineLength = line_lengths.most_common(1)[0][0]
 
-		for poemdir in os.listdir(path):
-			self.poems[poemdir] = open(os.path.join(path, poemdir, 'text')).read()
+        self.words = list()
+        self.words.append('\n')  # newline should be in the domain
+        nGramCounter = Counter()
+        for poem in self.poems.values():
+            split_poem = poem.split()
+            self.words += split_poem
+            lastNWords = []
+            for word in split_poem:
+                lastNWords.append(word)
+                if len(lastNWords) == ngramSize:
+                    nGramCounter[tuple(lastNWords)] += 1
+                    lastNWords = lastNWords[1:]
+        self.words = list(set(self.words))
 
-		self.words = list()
-		nGramCounter = Counter()
-		for poem in self.poems.values():
-			split_poem = poem.split()
-			self.words += split_poem
-			lastNWords = []
-			for word in split_poem:
-				lastNWords.append(word)
-				if len(lastNWords) == n:
-					nGramCounter[tuple(lastNWords)] += 1
-					lastNWords = lastNWords[1:]
+        self.subsets = []
+        for i in xrange(ngramSize + 1):
+            self.subsets.append(self.getNGrams(self.poems.values(), i))
 
-		self.words = list(set(self.words))
+    def getNGrams(self, poems, n):
+        retval = []
+        for poem in poems:
+            words = poem.split()
+            lastNWords = []
+            for word in words:
+                lastNWords.append(word)
+                if len(lastNWords) == n:
+                    retval.append(tuple(lastNWords))
+                    lastNWords = lastNWords[1:]
+        return retval
 
-		self.poemLength = 100 # todo
+    def addVariables(self):
+        for i in xrange(self.poemLength):
+            self.add_variable('w' + str(i), self.words)
 
-		self.subsets = []
-		for i in xrange(n+1):
-			self.subsets.append(self.getNGrams(self.poems.values(), i))
-		print self.subsets
+    def addLineLengthConstraints(self):
+        for i in xrange(0, self.poemLength, self.lineLength):
+            if i > 0:
+                self.add_unary_potential('w' + str(i), lambda x: x == '\n')
 
-	def getNGrams(self, poems, n):
-		retval = []
-		for poem in poems:
-			words = poem.split()
-			lastNWords = []
-			for word in words:
-				lastNWords.append(word)
-				if len(lastNWords) == n:
-					retval.append(tuple(lastNWords))
-					lastNWords = lastNWords[1:]
-		return retval
+    # amber
+    def addNGramFluencyConstraints(self):
+        pass
 
-	def addVariables(self):
-		for i in xrange(self.poemLength):
-			self.addVariable('w' + i, self.words)
+    def addNGramFactorFromNTuple(self, vars): # vars is tuple of n var names
+        for var in vars:
+            self.addVariable(''.join(vars) + var, )
 
-	def addLineLengthConstraints(self):
-		pass
+    # john
+    def addMoodFluencyConstraints(self):
+        pass
 
-	# amber
-	def addNGramFluencyConstraints(self):
-		pass
+    def addRhymeConstraints(self):
+        pass
 
-	def addNGramFactorFromNTuple(self, vars): # vars is tuple of n var names
-		for var in vars:
-			self.addVariable(''.join(vars) + var, )
-
-
-	# john
-	def addMoodFluencyConstraints(self):
-		pass
-
-	def addRhymeConstraints(self):
-		pass
-
-	def addMeterConstraints(self):
-		pass
+    def addMeterConstraints(self):
+        pass
 
 
 def main():
-	poemCSP = PoemCSP('./tmp', 5, 'Happy')
+    poemCSP = PoemCSP('./tmp', 5, 'Happy')
+    poemCSP.addVariables()
+    poemCSP.addLineLengthConstraints()
 
 if __name__ == "__main__":
-    main()
+  main()
