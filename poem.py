e0454a819a3a6a817684714caebd4167f4e861a4
Amber Rockwood <amberrose.rockwood@gmail.com>
2013-11-27 12:43:24 -0800

    Added syllable counter

--- poem.py
+++ poem.py
@@ -6,6 +6,7 @@ import random
 import solver
 from collections import Counter
 from collections import defaultdict
+import string, 	re
 
 def rhymes(w1, w2):
     l = min(len(w1), len(w2))
@@ -14,87 +15,135 @@ def rhymes(w1, w2):
     hammingDist = sum(ch1 != ch2 for ch1, ch2 in zip(w1, w2))
     return len(w1) > 3 and len(w2) > 3 and w1 != w2 and (float(hammingDist)/l < .25)
 
+syllable_count = Counter()
+
+subsyl = ["cial", "tia", "cius", "cious", "gui", "ion", "iou",
+                   "sia$", ".ely$"]
+
+addsyl = ["ia", "riet", "dien", "iu", "io", "ii",
+                   "[aeiouy]bl$", "mbl$",
+                   "[aeiou]{3}",
+                   "^mc", "ism$",
+                   "(.)(?!\\1)([aeiouy])\\2l$",
+                   "[^l]llien",
+                   "^coad.", "^coag.", "^coal.", "^coax.",
+                   "(.)(?!\\1)[gq]ua(.)(?!\\2)[aeiou]",
+                   "dnt$"]
+
+def syllables(wordList):
+	syllables = 0
+	for word in wordList:
+		word = word.strip().lower()
+		if syllable_count[word] > 0:
+			syllables += syllable_count[word]
+		else:
+			if word[-1] == 'e': # silent e!
+				word = word[:-1]
+
+			count = 0
+			prev_was_vowel = 0
+			for c in word:
+				is_vowel = c in ['a', 'e', 'i', 'o', 'u', 'y']
+				if is_vowel and not prev_was_vowel:
+					count += 1
+				prev_was_vowel = is_vowel
+
+			# Add & subtract syllables
+			for r in addsyl:
+				r = re.compile(r)
+				if r.search(word):
+					count += 1
+			for r in subsyl:
+				r = re.compile(r)
+				if r.search(word):
+					count -= 1
+
+			syllable_count[word] = count
+			syllables += count
+	return syllables
+
+
 class PoemCSP(solver.CSP):
-    def __init__(self, path, ngramSize, mood):
-	solver.CSP.__init__(self)
+	def __init__(self, path, ngramSize, mood):
+		solver.CSP.__init__(self)
 
-	self.ngramSize = ngramSize
+		self.ngramSize = ngramSize
 
-	self.poems = {}
-	num_lines = Counter()
-	line_lengths = Counter()
-        self.pos = defaultdict(set)
-	for poemdir in os.listdir(path):
-	    moods = open(os.path.join(path, poemdir, 'mood')).read().split()
-	    if mood in moods:
-		self.poems[poemdir] = \
-		    open(os.path.join(path, poemdir, 'text')).read().replace(
-		    '?', ' ?').replace('!', ' !').replace('.', ' .').replace(',', ' ,')
-		lines = self.poems[poemdir].split('\n')
-		num_lines[len(lines)] += 1
-		for line in lines:
-		    length = len(line.split())
-		    if length > 0:
-			line_lengths[length] += 1
-	self.lineLength = line_lengths.most_common(5)[random.randint(0, 4)][0]
-	self.numLines = num_lines.most_common(5)[random.randint(0, 4)][0] / 2
-	self.numNgrams = self.lineLength * self.numLines
+		self.poems = {}
+		num_lines = Counter()
+		line_lengths = Counter()
+	        self.pos = defaultdict(set)
+		for poemdir in os.listdir(path):
+		    moods = open(os.path.join(path, poemdir, 'mood')).read().split()
+		    if mood in moods:
+			self.poems[poemdir] = \
+			    open(os.path.join(path, poemdir, 'text')).read().replace(
+			    '?', ' ?').replace('!', ' !').replace('.', ' .').replace(',', ' ,')
+			lines = self.poems[poemdir].split('\n')
+			num_lines[len(lines)] += 1
+			for line in lines:
+			    length = len(line.split())
+			    if length > 0:
+				line_lengths[length] += 1
+		self.lineLength = line_lengths.most_common(5)[random.randint(0, 4)][0]
+		self.numLines = num_lines.most_common(5)[random.randint(0, 4)][0] / 2
+		self.numNgrams = self.lineLength * self.numLines
 
-	self.ngrams = self.getNGrams(self.poems.values(), ngramSize)
-	self.ngramCounter = Counter(self.ngrams)
-	self.domains = []
-	for _ in xrange(self.numLines):
-	    sample_index = int((len(self.ngrams)-self.lineLength*2) * random.random())
-	    self.domains.append(self.ngrams[sample_index:sample_index+self.lineLength*2])
+		self.ngrams = self.getNGrams(self.poems.values(), ngramSize)
+		self.ngramCounter = Counter(self.ngrams)
+		self.domains = []
+		for _ in xrange(self.numLines):
+		    sample_index = int((len(self.ngrams)-self.lineLength*2) * random.random())
+		    self.domains.append(self.ngrams[sample_index:sample_index+self.lineLength*2])
 
-	print self.lineLength, self.numLines, self.numNgrams
+		print self.lineLength, self.numLines, self.numNgrams
 
-    def getNGrams(self, poems, n):
-	if n == 0:
-	    return [()]
-	retval = []
-	for poem in poems:
-	    words = poem.split() + ['\n']
-	    lastNWords = []
-	    for word in words:
-		lastNWords.append(word)
-		if len(lastNWords) == n:
-		    retval.append(tuple(lastNWords))
-		    lastNWords = lastNWords[1:]
-	return retval
+	def getNGrams(self, poems, n):
+		if n == 0:
+		    return [()]
+		retval = []
+		for poem in poems:
+		    words = poem.split() + ['\n']
+		    lastNWords = []
+		    for word in words:
+			lastNWords.append(word)
+			if len(lastNWords) == n:
+			    retval.append(tuple(lastNWords))
+			    lastNWords = lastNWords[1:]
+		return retval
 
-    def addVariables(self):
-	for i in xrange(self.numNgrams):
-	    self.add_variable('v' + str(i), self.domains[i/self.lineLength])
+	def addVariables(self):
+		for i in xrange(self.numNgrams):
+		    self.add_variable('v' + str(i), self.domains[i/self.lineLength])
 
-    def addNGramFluencyConstraints(self):
-	for i in xrange(1, self.numNgrams):
-	    if (i-1)/self.lineLength == i/self.lineLength:
-		def line_consistency(v1, v2):
-		    for i in xrange(1, len(v1)):
-			if v1[i] != v2[i-1]:
+	def addNGramFluencyConstraints(self):
+		for i in xrange(1, self.numNgrams):
+		    if (i-1)/self.lineLength == i/self.lineLength:
+			def line_consistency(v1, v2):
+			    for i in xrange(1, len(v1)):
+				if v1[i] != v2[i-1]:
+				    return False
+				return True
+			self.add_binary_potential('v' + str(i-1), 'v' + str(i), line_consistency)
+		for i in xrange(self.numNgrams):
+		    self.add_unary_potential('v' + str(i), lambda x: self.ngramCounter[x])
+		    self.add_unary_potential('v' + str(i), lambda x: 'by' not in x)
+		    for j in xrange(i+1,self.numNgrams):
+			self.add_binary_potential('v' + str(i), 'v' + str(j), lambda x, y: x != y)
+		self.add_unary_potential('v0', lambda x: x[0] not in [',', '.', '?', '!'])
+		def capitalized(x):
+		    if not x[0][0].isupper():
+			return False
+		    for i in xrange(1, len(x)):
+			if x[i][0].isupper():
 			    return False
-			return True
-		self.add_binary_potential('v' + str(i-1), 'v' + str(i), line_consistency)
-	for i in xrange(self.numNgrams):
-	    self.add_unary_potential('v' + str(i), lambda x: self.ngramCounter[x])
-	    self.add_unary_potential('v' + str(i), lambda x: 'by' not in x)
-	    for j in xrange(i+1,self.numNgrams):
-		self.add_binary_potential('v' + str(i), 'v' + str(j), lambda x, y: x != y)
-	self.add_unary_potential('v0', lambda x: x[0] not in [',', '.', '?', '!'])
-	def capitalized(x):
-	    if not x[0][0].isupper():
-		return False
-	    for i in xrange(1, len(x)):
-		if x[i][0].isupper():
-		    return False
-	    for i in xrange(1, len(x[0])):
-		if x[0][i].isupper():
-		    return False
-	    return True
-	self.add_unary_potential('v0', capitalized)
-	self.add_unary_potential('v' + str(self.numNgrams-1),
-				 lambda x: x[self.ngramSize-1] in ['.', '?', '!'])
+		    for i in xrange(1, len(x[0])):
+			if x[0][i].isupper():
+			    return False
+		    return True
+		self.add_unary_potential('v0', capitalized)
+		self.add_unary_potential('v' + str(self.numNgrams-1),
+					 lambda x: x[self.ngramSize-1] in ['.', '?', '!'])
 
 def main():
     alg = solver.BacktrackingSearch()
